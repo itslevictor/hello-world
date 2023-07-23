e5797faaf90ad80d138252928da9a2be13ee77ff
Victor Fernando <adm.victorfernando@gmail.com>
2023-07-23 15:09:05 -0300

    Primeiro Commit IFOOD - CUBOS

---
+++ hello-world.js
@@ -0,0 +1,12 @@
+// ARQUIVO DE ESTUDO VICTOR - BOLSA IFOOD 1000 - CUBOS ACADEMY
+<!DOCTYPE html>
+<html>
+  <body>
+    <p><b>Ola Mundo</b></p>
+    <p>Meu hello world para cubos academy<p>
+    <p><i>Victor Fernando</i></p>
+    <script>
+      alert('ola mundo');
+    </script>
+  </body>
+</html>
