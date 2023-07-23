850e3de200a538f95a038cf03a5e17e1a9933111
Victor Fernando <adm.victorfernando@gmail.com>
2023-07-23 15:11:17 -0300

    Adicionando exclamação e colocando um ponto e virgula no fim

--- hello-world.js
+++ hello-world.js
@@ -1,12 +1,2 @@
 // ARQUIVO DE ESTUDO VICTOR - BOLSA IFOOD 1000 - CUBOS ACADEMY
-<!DOCTYPE html>
-<html>
-  <body>
-    <p><b>Ola Mundo</b></p>
-    <p>Meu hello world para cubos academy<p>
-    <p><i>Victor Fernando</i></p>
-    <script>
-      alert('ola mundo');
-    </script>
-  </body>
-</html>
+console.log("olá mundo!");
